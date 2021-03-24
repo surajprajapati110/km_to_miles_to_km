24f69e0742505a03cff1b9f21ffedb7d4930223b
Suraj prajapati <79505595+surajprajapati110@users.noreply.github.com>
2021-03-25 01:06:59 +0530

    Add files via upload

---
+++ kmtomiles1.py
@@ -0,0 +1,4 @@
+km = float(input("Enter value in kilometers: "))
+miles =  1.609344
+miles = km / miles
+print(km, "km =", round(miles,4), "miles")
---
+++ milestokm.py
@@ -0,0 +1,4 @@
+miles = float(input("Enter value in miles: "))
+km=  1.609344
+km = miles * km 
+print(miles, "miles =", round(km,3), "km")
